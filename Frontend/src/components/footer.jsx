c5aa318fdb893cf984f9db2121cdf0cbf1115205
Naol Kecha <naolkecha@gmail.com>
2024-02-02 15:47:20 +0300

    n

--- Frontend/src/components/footer.jsx
+++ Frontend/src/components/footer.jsx
@@ -49,6 +49,8 @@ return (
                 <div className="col-md-6 text-center text-md-end">
                     Designed By <a href="https://naolkecha.com">Naol Kecha</a>
 
+
+
                 </div>
             </div>
         </div>
